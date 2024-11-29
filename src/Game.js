df73db65afd08b95078c3b0cc3d86cfe05e07f3d
hao.chen8 <hc222ig@student.lnu.se>
2024-11-29 14:58:16 +0100

    Red: Game implementation fail

---
+++ src/Game.js
@@ -0,0 +1,43 @@
+/**
+ * @file Defines the Game class.
+ * @module Game
+ * @author Hao Chen
+ * @version 1.0.0
+ */
+
+import { Player } from './Player.js'
+import { Dice } from './Dice.js'
+
+/**
+ * Represents a game.
+ */
+export class Game {
+  
+    /**
+    * Create a game.
+    * @param {Player[]} players - The players.
+    * @param {Dice} dice - The dice.
+    */
+    constructor(players, dice) {
+      this.players = players
+      this.dice = dice
+    }
+
+    /**
+     * Play the game.
+     * @returns {Player} The winner.
+     */
+    play() {
+      let highestScore = 0
+      let winner = null
+
+      this.players.forEach(player => {
+        const roll = this.dice.roll()
+        if (roll > highestScore) {
+          highestScore = roll
+          winner = player
+        }
+      })
+      return winner
+    }
+}
